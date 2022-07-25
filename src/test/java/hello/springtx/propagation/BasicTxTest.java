1a882ba7bf3d050cbef29c15f25e416f24a71a07
hyejin1231 <phj981231@naver.com>
2022-07-26 08:01:55 +0900

    20220726 스프링 트랜잭션 전파1 - 기본 커밋,롤백

---
+++ src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -0,0 +1,52 @@
+package hello.springtx.propagation;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
+
+import javax.sql.DataSource;
+
+@Slf4j
+@SpringBootTest
+public class BasicTxTest {
+
+    @Autowired
+    PlatformTransactionManager txManager;
+
+    @TestConfiguration
+    static class Config {
+        // 원래는 스프링이 트랜잭션 매니저도 등록을 해주는데 직접 등록함
+        @Bean
+        public PlatformTransactionManager transactionManager(DataSource dataSource) {
+            return new DataSourceTransactionManager(dataSource);
+        }
+    }
+
+    @Test
+    void commit() {
+        log.info("트랜잭션 시작");
+        TransactionStatus status = txManager.getTransaction(new DefaultTransactionAttribute()); // 트랜잭션 시작
+
+        log.info("트랜잭션 커밋 시작");
+        txManager.commit(status);
+        log.info("트랜잭션 커밋 완료");
+    }
+
+    @Test
+    void rollback() {
+        log.info("트랜잭션 시작");
+        TransactionStatus status = txManager.getTransaction(new DefaultTransactionAttribute()); // 트랜잭션 시작
+
+        log.info("트랜잭션 롤백 시작");
+        txManager.rollback(status);
+        log.info("트랜잭션 롤백 완료");
+    }
+
+}
