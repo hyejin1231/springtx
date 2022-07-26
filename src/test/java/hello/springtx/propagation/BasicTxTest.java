7013f425d72f0cace0be3b10840cf4ff410271a0
hyejin1231 <phj981231@naver.com>
2022-07-26 09:04:46 +0900

    20220726 스프링 트랜잭션 전파1 - 기본 전파 기본, 예제

--- src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -10,6 +10,7 @@ import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import javax.sql.DataSource;
 
@@ -73,6 +74,23 @@ public class BasicTxTest {
         TransactionStatus tx2 = txManager.getTransaction(new DefaultTransactionAttribute()); // 트랜잭션 시작
         log.info("트랜잭션2 롤백");
         txManager.rollback(tx2);
+    }
+
+    @Test
+    void inner_commit() {
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute()); // 외부 트랜잭션
+        log.info("outer.isNewTransaction()={}", outer.isNewTransaction()); // 이게 처음 시작된 트랜잭션인가? 물어보는 메서드
+
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("inner.isNewTransaction()={}", inner.isNewTransaction());
+        log.info("내부 트랜잭션 커밋");
+        txManager.commit(inner);
+
+
+        log.info("외부 트랜잭션 커밋");
+        txManager.commit(outer);
     }
 
 }
